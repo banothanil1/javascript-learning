912c184b151957e19caecaaff8d0e1cd2787fe4e
Banoth Anil Nayak <98812186+banothanil1@users.noreply.github.com>
2024-07-08 18:49:22 +0000

    variables session is completed

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const anilNayak="my name is anil";
+
+let sunilNayak=32234234324323442343324;
+var minnuBandari="minnu";
+
+swarupa=23;
+
+let himanshu=null
+
+/*
+prefer not to use the var keyword becouse it may be changed by anyone as it has issue with scope
+*/
+
+//null is a object exmaple can be temparature 
+
+//symbol datatype is used in the react to uniquely identify the layouts etc
+let rajuNayak;
+//console.table([anilNayak,sunilNayak,minnuBandari,swarupa,rajuNayak]);
+
+console.log(typeof himanshu);
