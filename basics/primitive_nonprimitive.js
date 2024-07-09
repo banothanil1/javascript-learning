5ff8a996ee011d252b6af185960602925a2d5805
Banoth Anil Nayak <98812186+banothanil1@users.noreply.github.com>
2024-07-09 07:02:35 +0000

    primitives and non primitives completed

---
+++ basics/primitive_nonprimitive.js
@@ -0,0 +1,23 @@
+
+//non primitive data types
+
+//arrays,objects ,functions
+
+let anil=[1,2,3,4];//arrays are stored in []
+
+const object={
+    name:"anil nayak banoth",
+    erp: 200303124135,
+    college:"parul university"
+}
+
+const sunilNayak=function(){
+    console.log("this is my first function defined over here");
+}
+
+console.log(typeof sunilNayak);
+
+console.log(typeof anil);
+
+console.log(typeof object);
+
--- basics/type_conversion.js
+++ basics/type_conversion.js
@@ -1,1 +1,12 @@
 //its a simple thing not required to code for my knowledge level
+//sometime in javascript nulll gets converted into the 0 and sometime it gets converted to the Nan that is the biggest problem we have 
+
+//datatypes
+//-------------------------
+/*
+*primitives =>string ,number,boolean ,null ,undefined ,symbol
+
+*array,objects ,functions
+
+*
+*/
