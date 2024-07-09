805740663335c0cc47ec99c188f928d888c57af6
Banoth Anil Nayak <98812186+banothanil1@users.noreply.github.com>
2024-07-09 17:35:05 +0000

    stack and heap and strings

---
+++ basics/stack_heap.js
@@ -0,0 +1,18 @@
+let object1={
+    name:"anil nayak banoth",
+    college:"parul university",
+}
+
+let object2={
+    name:"sunil nayak banoth",
+    college:"geetham university",
+}
+
+ object1=object2;
+
+ object1.name="kiran nayak banoth" 
+ object1.college="geetha high school"
+
+ console.log(object1);
+
+ console.log(object2);
---
+++ basics/strings.js
@@ -0,0 +1,9 @@
+/*using the string concatenartion like how we used to  do in the java using + operated is outdated
+//use the string interpolation like below
+*/
+
+let name="Banoth Anil Nayak";
+let collegeName="geetham university";
+
+let hello=`good morning my name ${name} and college name is ${collegeName}`; //this is known as string interpolation
+console.log(hello);
